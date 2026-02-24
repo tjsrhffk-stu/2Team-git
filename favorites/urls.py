176e5414b43b5ed91e754a498eac1af43b9e3956
gustjr1 <ohhunsuk1405@gmail.com>
2026-02-24 14:35:23 +0900

    Remove cached files

--- favorites/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "favorites"
-
-urlpatterns = [
-    path("toggle/<int:restaurant_id>/", views.toggle_favorite, name="toggle"),
-    path("", views.favorite_list, name="list"),
-]
--- users/utils.py
+++
@@ -1,7 +0,0 @@
-# users/utils.py
-def is_owner(user) -> bool:
-    """
-    사장 여부 체크
-    - OwnerProfile이 있으면 True
-    """
-    return hasattr(user, "owner_profile")
