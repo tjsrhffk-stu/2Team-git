dc5457533f96893ee40d9641a16d3baff59de528
tjsrhffk-stu <tjsrhffk@gmail.com>
2026-02-26 13:01:20 +0900

    Fix: static 폴더 구조 최적화 및 음식점 등록/수정 CSS 적용 완료

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
